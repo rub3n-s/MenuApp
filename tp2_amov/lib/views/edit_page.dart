137a9aea9a49ebebf666de0964d95ca324b7d26d
Rúben Santos <lcs_ruben@hotmail.com>
2022-12-28 22:39:25 +0000

    column scrollable

--- tp2_amov/lib/views/edit_page.dart
+++ tp2_amov/lib/views/edit_page.dart
@@ -24,78 +24,80 @@ class _EditPageState extends State<EditPage> {
         body: Center(
           child: Container(
             padding: const EdgeInsets.all(15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                  child: TextFormField(
-                      decoration: const InputDecoration(
-                          labelText: 'Soup: ', border: OutlineInputBorder()),
-                      initialValue: _menu.original.soup,
-                      maxLines: null),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                // Meat
-                SizedBox(
-                  width: double.infinity,
-                  child: TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Meat: ',
-                        border: OutlineInputBorder(),
-                      ),
-                      initialValue: _menu.original.meat,
-                      maxLines: null),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                // Fish
-                SizedBox(
-                  width: double.infinity,
-                  child: TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Fish: ',
-                        border: OutlineInputBorder(),
-                      ),
-                      initialValue: _menu.original.fish,
-                      maxLines: null),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                // Vegetarian
-                SizedBox(
-                  width: double.infinity,
-                  child: TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Vegetarian: ',
-                        border: OutlineInputBorder(),
-                      ),
-                      initialValue: _menu.original.vegetarian,
-                      maxLines: null),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                // Desert
-                SizedBox(
-                  width: double.infinity,
-                  child: TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Desert: ',
-                        border: OutlineInputBorder(),
-                      ),
-                      initialValue: _menu.original.desert,
-                      maxLines: null),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                ElevatedButton(onPressed: () {}, child: const Text('Confirm'))
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Soup: ', border: OutlineInputBorder()),
+                        initialValue: _menu.original.soup,
+                        maxLines: null),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  // Meat
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Meat: ',
+                          border: OutlineInputBorder(),
+                        ),
+                        initialValue: _menu.original.meat,
+                        maxLines: null),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  // Fish
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Fish: ',
+                          border: OutlineInputBorder(),
+                        ),
+                        initialValue: _menu.original.fish,
+                        maxLines: null),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  // Vegetarian
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Vegetarian: ',
+                          border: OutlineInputBorder(),
+                        ),
+                        initialValue: _menu.original.vegetarian,
+                        maxLines: null),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  // Desert
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Desert: ',
+                          border: OutlineInputBorder(),
+                        ),
+                        initialValue: _menu.original.desert,
+                        maxLines: null),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: const Text('Confirm'))
+                ],
+              ),
             ),
           ),
         ));
