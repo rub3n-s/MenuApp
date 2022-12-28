c68e596773642269e0bb03c64f32c1263ba2334a
Rúben Santos <lcs_ruben@hotmail.com>
2022-12-28 23:12:09 +0000

    utf8 fix

--- tp2_amov/lib/services/remote_service.dart
+++ tp2_amov/lib/services/remote_service.dart
@@ -1,14 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../models/menu.dart';
 
 class RemoteService {
   Future<Menu?> getMenu() async {
-    var client = http.Client();
+    /*var client = http.Client();
     var uri = Uri.parse('http://10.0.2.2:8080/menu');
-    var response = await client.get(uri);
+    var response = await client.get(
+      uri,
+      headers: {"Content-Type": "application/json"},
+    ); */
+
+    http.Response response = await http.get(
+        Uri.parse('http://10.0.2.2:8080/menu'),
+        headers: {'Content-Type': 'application/json'});
     if (response.statusCode == 200) {
-      var json = response.body;
-      return menuFromJson(json);
+      return menuFromJson(utf8.decode(response.bodyBytes));
     }
     return null;
   }
