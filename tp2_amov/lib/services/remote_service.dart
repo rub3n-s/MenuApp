0e8b447eb3aeb5416979baae734f8da290029dd8
Rúben Santos <lcs_ruben@hotmail.com>
2022-12-28 23:12:41 +0000

    remote fix

--- tp2_amov/lib/services/remote_service.dart
+++ tp2_amov/lib/services/remote_service.dart
@@ -6,13 +6,6 @@ import '../models/menu.dart';
 
 class RemoteService {
   Future<Menu?> getMenu() async {
-    /*var client = http.Client();
-    var uri = Uri.parse('http://10.0.2.2:8080/menu');
-    var response = await client.get(
-      uri,
-      headers: {"Content-Type": "application/json"},
-    ); */
-
     http.Response response = await http.get(
         Uri.parse('http://10.0.2.2:8080/menu'),
         headers: {'Content-Type': 'application/json'});
